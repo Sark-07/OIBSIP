bbf2f72b30d8e6c4e5d687775a4a428332fd06c2
Prime <iampritamsarkar07@gmail.com>
2023-04-20 02:46:18 +0530

    Edited todo-list

--- Todo-list/src/app.js
+++ Todo-list/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const router  = require('../routes/router');
+const router = require('../routes/router');
 const connectDB = require('../database/connectDB');
 const cors = require('cors');
 require('dotenv/config');
@@ -9,18 +9,18 @@ const port = process.env.PORT || 3000;
 // cors policy
 
 app.use(cors(
-    {origin: '*'}
+    { origin: '*' }
 ))
 
 // parsing all json and url encoded data
 app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 // middleware for the routes
 app.use('/api/v1/items', router)
 
-// app.use(express.static('./public'))
+app.use(express.static('./public'))
 
 //  route 404
 app.all('*', (req, res) => {
@@ -29,10 +29,10 @@ app.all('*', (req, res) => {
 })
 
 const connectDatabase = async () => {
-    await connectDB(process.env.Mongo_URI).then( () => {
+    await connectDB(process.env.Mongo_URI).then(() => {
         console.log('connected to db...');
-    }).catch( (err) => console.log(err));
-    app.listen(port, () => console.log("Listening to port 3000..."));
+    }).catch((err) => console.log(err));
+    app.listen(port, () => console.log(`Listening to port ${port}...`));
 }
 connectDatabase()
 
